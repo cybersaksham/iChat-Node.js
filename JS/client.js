cc4bf35976ee264ee4d353805e2b64cb8a37fed2
Saksham <2020UCP1801@mnit.ac.in>
2021-09-11 09:01:12 +0530

    basic socket connection

--- JS/client.js
+++ JS/client.js
@@ -3,4 +3,7 @@ const msgContainer = document.getElementById("msgContainer");
 const form = document.getElementById("sendForm");
 const msgInp = document.getElementById("msgInp");
 
+// Socket IO
+const socket = io("http://localhost:8000");
+
 // const name = prompt("Enter your name to join chat.");
