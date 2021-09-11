f5545b305a06b376373725b52947cdfc235cd4b8
Saksham <2020UCP1801@mnit.ac.in>
2021-09-11 08:54:36 +0530

    basic socket start

---
+++ JS/client.js
@@ -0,0 +1,6 @@
+// Getting DOM Elements
+const msgContainer = document.getElementById("msgContainer");
+const form = document.getElementById("sendForm");
+const msgInp = document.getElementById("msgInp");
+
+// const name = prompt("Enter your name to join chat.");
